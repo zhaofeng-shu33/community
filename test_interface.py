b9ae41ec59dfcf18b4ec982c0e45e2223ef83539
zhaofeng-shu33 <616545598@qq.com>
2019-09-15 11:21:22 +0800

    add test karate dataset

--- test_interface.py
+++ test_interface.py
@@ -4,7 +4,7 @@ from GN import GN
 
 class TestGN(unittest.TestCase):
     def test_6point(self):
-        G=nx.Graph()
+        G = nx.Graph()
         G.add_edge(0, 2)
         G.add_edge(0, 1)
         G.add_edge(2, 1)
@@ -15,6 +15,16 @@ class TestGN(unittest.TestCase):
         gn = GN()
         gn.fit(G)
         self.assertEqual(gn.Bestcomps, [{0, 1, 2}, {3, 4, 5}])
+        print(gn.tree)
+
+    def test_karate_dataset(self):
+        G = nx.readwrite.gml.read_gml('karate.gml', label=None)
+        n_nodes = len(G.nodes)
+        # relabel nodes, starting from zero
+        mapping = {i+1:i for i in range(n_nodes)}
+        G = nx.relabel_nodes(G, mapping)
+        gn = GN()
+        gn.fit(G)
         print(gn.tree)
 
 if __name__ == '__main__':
